e0ff8e6aef11b5a0944b091b16af6be6df6f5faa
jk7800124-art <jk7800124@gmail.com>
2026-06-19 19:38:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    base: '/PAAN-STUDIO/',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './'),
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {
+      VITE_GEMINI_API_KEY: "AQ.Ab8RN6JaJnn0pzMxr6JHN7bY2Br3Jpbjbo_Gu-G8YN9SlBJtpQ",
+      GEMINI_API_KEY: "AQ.Ab8RN6JaJnn0pzMxr6JHN7bY2Br3Jpbjbo_Gu-G8YN9SlBJtpQ"
     },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modify/add/file watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
-      // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+    'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify("AQ.Ab8RN6JaJnn0pzMxr6JHN7bY2Br3Jpbjbo_Gu-G8YN9SlBJtpQ"),
+    'import.meta.env.GEMINI_API_KEY': JSON.stringify("AQ.Ab8RN6JaJnn0pzMxr6JHN7bY2Br3Jpbjbo_Gu-G8YN9SlBJtpQ")
+  }
 });
